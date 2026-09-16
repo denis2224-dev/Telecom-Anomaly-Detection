cf10eb66d858871393b26735a2b8c49185ca18c5
Denis <denymoroz2006@gmail.com>
2026-09-16 09:46:57 +0300

    feat(incident-service): add initial database schema

--- services/incident-service/src/test/java/md/utm/telecom/incidents/TestcontainersConfiguration.java
+++ services/incident-service/src/test/java/md/utm/telecom/incidents/TestcontainersConfiguration.java
@@ -19,7 +19,9 @@ class TestcontainersConfiguration {
 	@Bean
 	@ServiceConnection
 	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+		return new PostgreSQLContainer(DockerImageName.parse("postgres:16.4-alpine"))
+				.withDatabaseName("incidents_db")
+				.withInitScript("db/context-test-init.sql");
 	}
 
 }
